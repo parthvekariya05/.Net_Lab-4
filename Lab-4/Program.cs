6b76e56e20799f1a6b9dda5255678c5f786acb56
parthvekariya05 <23010101660@darshan.ac.in>
2024-06-24 10:49:58 +0530

    Add project files.

---
+++ Lab-4/Program.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+
+//ArrayList a1 = new ArrayList();
+
+//a1.Add("Parth");
+//a1.Add("Jay");
+//a1.Add("Jayesh");
+//a1.Add("Mohit");
+//a1.Add("Mudit");
+
+//Console.WriteLine("All student : ");
+//foreach (string i in a1)
+//{
+    
+//    Console.WriteLine(i);
+//}
+//Console.WriteLine("Jay is Remove");
+//a1.Remove("Jay");
+//foreach (string i in a1)
+//{
+    
+//    Console.WriteLine(i);
+//}
+//Console.WriteLine("Range Remove");
+//a1.RemoveRange(1, 3);
+//foreach (string i in a1)
+//{
+   
+//    Console.WriteLine( i);
+//}
+//Console.WriteLine("Arry is clear");
+//a1.Clear();
+//foreach (string i in a1)
+//{
+        
+//    Console.WriteLine( i);
+//}
+
+
+//List<string> student = new List<string>();
+//student.Add("Parth");
+//student.Add("Jay");
+//student.Add("Jayesh");
+//student.Add("Mohit");
+//student.Add("Mudit");
+//Console.WriteLine("All student : ");
+//foreach (string i in student)
+//{
+//    Console.WriteLine(i);
+//}
+//Console.WriteLine("----------------------------------------------");
+//Console.WriteLine("Jay is Remove");
+//student.Remove("Jay");
+//foreach (string i in student)
+//{
+
+//    Console.WriteLine(i);
+//}
+//Console.WriteLine("----------------------------------------------");
+//Console.WriteLine("Range Remove");
+//student.RemoveRange(1, 3);
+//foreach (string i in student)
+//{
+
+//    Console.WriteLine(i);
+//}
+//Console.WriteLine("----------------------------------------------");
+//Console.WriteLine("Arry is clear");
+//student.Clear();
+//foreach (string i in student)
+//{
+
+//    Console.WriteLine(i);
+//}
+
+//Stack s1 = new Stack();
+//s1.Push(10);
+//s1.Push(20);
+//s1.Push(30);
+//s1.Push(40);
+//s1.Push(50);
+//Console.WriteLine("All element : ");
+//foreach (var i in s1)
+//{
+//    Console.WriteLine(i);
+//}
+//Console.WriteLine("----------------------------------------------");
+//Console.WriteLine("first element is Remove");
+//s1.Pop();
+//foreach (var i in s1)
+//{
+
+//    Console.WriteLine(i);
+//}
+//Console.WriteLine("----------------------------------------------");
+//Console.WriteLine("first element is return"+s1.Peek());
+//Console.WriteLine("----------------------------------------------");
+//Console.WriteLine("element is contain :"+ s1.Contains(40));
+//Console.WriteLine("----------------------------------------------");
+//Console.WriteLine("Stack is clear");
+//s1.Clear();
+//foreach (string i in s1)
+//{
+
+//    Console.WriteLine(i);
+//}
+
+//Queue q1 = new Queue();
+//q1.Enqueue(10);
+//q1.Enqueue(20);
+//q1.Enqueue(30);
+//q1.Enqueue(40);
+//q1.Enqueue(50);
+//Console.WriteLine("All element : ");
+//foreach (var i in q1)
+//{
+//    Console.WriteLine(i);
+//}
+//Console.WriteLine("----------------------------------------------");
+//Console.WriteLine("20 element is Remove :");
+//q1.Enqueue(20);
+//foreach (var i in q1)
+//{
+
+//    Console.WriteLine(i);
+//}
+//Console.WriteLine("----------------------------------------------");
+//Console.WriteLine("first element is return" + q1.Peek());
+//Console.WriteLine("----------------------------------------------");
+//Console.WriteLine("element is contain :" + q1.Contains(40));
+//Console.WriteLine("----------------------------------------------");
+//Console.WriteLine("Stack is clear");
+//q1.Clear();
+//foreach (string i in q1)
+//{
+
+//    Console.WriteLine(i);
+//}
